87c31715e51a4636b1c540315e3a4ea06c370b46
s@1996 <54677210+9061711428@users.noreply.github.com>
2019-12-12 14:21:48 +0530

    Update Abstractclass.java

--- Abstractclass.java
+++ Abstractclass.java
@@ -1,8 +1,8 @@
 package abstraction;
 
-abstract class Abstractclass {
+abstract class Abstractclass {  //Abstract class
 	
-public abstract void add(int a,int b); 
+public abstract void add(int a,int b); //Abstract method
 public void sub(int a,int b)
 {
 	System.out.println((a-b));
